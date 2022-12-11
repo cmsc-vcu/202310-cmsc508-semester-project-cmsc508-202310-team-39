ba08c52f1624c424fb9d6065b08774897f164c0f
bluearevalo <90929912+bluearevalo@users.noreply.github.com>
2022-12-10 21:04:38 -0500

    beginning of queries

--- Phase2/public_html/index.php
+++ Phase2/public_html/index.php
@@ -1,1 +1,28 @@
-testing testing 1 2 3
+<html>
+<head>
+    <title>My Queries</title>
+</head>
+<body>
+    <h1>My Queries</h1>
+    <h2>Query 1: How many streamers average over 10,000 viewers?</h2>
+    <p>
+        INSERT QUERY HERE
+    </p>
+    <h2>Query 2: Display a list of each game genre and their total current viewers</h2>
+    <p>
+        INSERT QUERY HERE
+    </p>
+    <h2>Query 3: What were the top 3 most common languages spoken in each stream?</h2>
+    <p>
+        INSERT QUERY HERE
+    </p>
+    <h2>Query 4: Display a list of each Streamer's age and their number of followers</h2>
+    <p>
+        INSERT QUERY HERE
+    </p>
+    <h2>Query 5: What were the top 2 oldest games streamed?</h2>
+    <p>
+        INSERT QUERY HERE
+    </p>
+</body>
+</html>
