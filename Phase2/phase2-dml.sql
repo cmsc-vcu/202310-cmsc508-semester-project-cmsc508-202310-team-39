efdc3f35fe01b9c3f31afc436e9d9202e8d36b02
untermanlm <80475070+untermanlm@users.noreply.github.com>
2022-12-10 19:59:41 -0500

    Update phase2-dml.sql

--- Phase2/phase2-dml.sql
+++ Phase2/phase2-dml.sql
@@ -1,3 +1,12 @@
 -- phase2-dml.sql
 
--- query 1: display a list of each streamer and their most streamed Twitch category
+-- query 1: How many streamers average over 10,000 viewers?
+
+SELECT
+    name,
+    avg_viewers
+FROM Streamer a
+    join Stream b on (name = streamer_name)
+WHERE
+    avg_viewers >= 10000
+;
