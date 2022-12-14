36504e161c25414ee22ffc4db65eea76c64e1b4f
bluearevalo <90929912+bluearevalo@users.noreply.github.com>
2022-12-14 16:37:44 -0500

    triggers 3/5

--- Phase2/phase2-ddl.sql
+++ Phase2/phase2-ddl.sql
@@ -36,17 +36,17 @@ create table stream(
 );
 create table category(
     category_name varchar(255),
-    category_type varchar(255) unique,
     position int not null,
     avg_category_viewers float not null,
     peak_category_viewers int not null,
     start_time time not null,
     duration_in_hours int not null,
     streamID int not null,
-    check (category_type in ('Personal', 'Gaming')),
     primary key (streamID, start_time),
     foreign key (streamID) references stream(streamID)
 );
+
+-- meant to be a disjoint set of category, but couldn't figure out how to actaully connect them by non unique or primary key
 create table game(
     game_name varchar(255) unique,
     game_genre varchar(255),
@@ -54,9 +54,10 @@ create table game(
     game_popularity_rank int not null unique,
     game_release_date Date not null,
     game_category_type varchar(255),
+    streamID int not null,
     primary key(game_name),
-    foreign key(game_category_type) references category(category_type),
-    check (game_category_type = 'Gaming')
+    foreign key (streamID) references stream(streamID),
+    check (game_name != 'Just Chatting' and game_name != 'IRL')
 );
 create table chat(
     chat_ID int not null AUTO_INCREMENT, 
@@ -66,19 +67,6 @@ create table chat(
     primary key (chat_ID),
     foreign key (streamID) references stream(streamID)
 );
-
--- Create a trigger that updates the `follower_count` and `subscriber_count`
--- in the `Streamer` table when a new stream is inserted into the `stream` table
-CREATE TRIGGER update_streamer_counts
-AFTER INSERT ON Stream
-FOR EACH ROW
-BEGIN
-    UPDATE Streamer
-    SET follower_count = follower_count + NEW.followers_gained,
-        subscriber_count = subscriber_count + NEW.subscribers_gained
-    WHERE name = NEW.streamer_name;
-END;
-
 
 
 -- task 2: insert values
@@ -95,22 +83,24 @@ insert into stream(streamer_name, start_date, title, avg_viewers, peak_viewers, 
     ('pokimane', '2022-12-10 12:00:05', 'Valorant!', 20000, 23000, 4000, 500, 'English'),
     ('hasanabi', '2022-12-10 11:08:10', 'Politics and shit', 30000, 50000, 3000, 500, 'Turkish'),
     ('erobb221', '2022-12-10 17:24:12', 'not ending stream until i land on mars', 6000, 7000, 1000, 100, 'French'),
-    ('clintstevens', '2022-12-10 18:00:00', 'Welcome to the stream!', 4000, 6000, 1000, 200, 'English')
+    ('clintstevens', '2022-12-10 18:00:00', 'Welcome to the stream!', 4000, 6000, 1000, 200, 'English'),
+    ('clintstevens', '2022-12-14 18:00:00', 'Polybridge', 4000, 6000, 1000, 200, 'English')
 ;
 insert into category (streamID, category_name, position, avg_category_viewers, peak_category_viewers, start_time, duration_in_hours) values
     (1, 'Just Chatting', 2, 70000, 100000, '22:06:30', 1),
     (1, 'Minecraft', 1, 75000, 78000, '23:06:30', 5),
     (2, 'Just Chatting', 2, 80000, 95000, '20:04:01', 8),
     (3, 'Valorant', 1, 200000, 400000, '16:00:00', 5),
-    (4, 'Kerbal Space Program', 1, 200000, 400000, '16:00:00', 5),
-    (5, 'Super Mario 64', 1, 200000, 400000, '16:00:00', 5)
+    (4, 'Hearthstone', 1, 200000, 400000, '16:00:00', 5),
+    (5, 'Super Mario 64', 1, 200000, 400000, '16:00:00', 5),
+    (6, 'Polybridge', 4, 5000, 10000, '18:00:00', 4)
 ;
-insert into game(game_name, game_genre, total_current_viewers, game_popularity_rank, game_release_date) values 
-    ('Minecraft', 'Sandbox', 100000, 5, '2011-11-18'),
-    ('Grand Theft Auto V', 'Open World', 100000, 4, '2013-9-17'),
-    ('Hearthstone', 'TCG', 100000, 2, '2014-11-3'),
-    ('Fallout 4', 'Survival', 100000, 3, '2015-11-10'),
-    ('Polybridge', 'Puzzle', 100000, 1, '2015-6-30')
+insert into game(game_name, game_genre, total_current_viewers, game_popularity_rank, game_release_date, streamID) values 
+    ('Minecraft', 'Sandbox', 100000, 5, '2011-11-18', 1),
+    ('Valorant', 'Shooter', 100000, 4, '2013-9-17', 3),
+    ('Hearthstone', 'TCG', 100000, 2, '2014-11-3', 4),
+    ('Super Mario 64', 'Platformer', 100000, 3, '2015-11-10', 5),
+    ('Polybridge', 'Puzzle', 100000, 1, '2015-6-30', 6)
 ;
 insert into chat(streamID, num_chatters, unique_messages) values 
     (1, 70000, 200000),
@@ -120,11 +110,114 @@ insert into chat(streamID, num_chatters, unique_messages) values
     (5, 6000, 8000)
 ;
 
---for you to test 
-SHOW TRIGGERS;
+-- task 3: drop list for views
+drop view if exists streamer_view;
+drop view if exists stream_view;
+drop view if exists category_view;
+drop view if exists chat_view;
 
-SELECT * FROM Streamer;
+-- task 4: create views
 
-SELECT name, follower_count, subscriber_count
-FROM Streamer
-WHERE name = 'xQc';
+create view streamer_view as (
+    SELECT
+        name,
+        age, 
+        account_creation_date
+    FROM
+        streamer
+    ORDER BY
+        name
+);
+create view stream_view as (
+    SELECT
+        streamer_name,
+        start_date,
+        title
+    FROM
+        stream
+    ORDER BY
+        start_date
+);
+create view category_view as (
+    SELECT
+        streamer_name,
+        category_name,
+        start_date,
+        start_time
+    FROM
+        stream a
+            join category b on (a.streamID = b.streamID)
+);
+create view chat_view as (
+    SELECT
+        num_chatters,
+        unique_messages
+    FROM 
+        chat
+);
+
+-- task 5: drop if exists procedures
+
+drop procedure if exists GetStreamerInfo;
+drop procedure if exists GetStreamInfo;
+drop procedure if exists GetCategoryInfo;
+drop procedure if exists GetGameInfo;
+drop procedure if exists GetChatInfo;
+
+
+-- task 6: create procedures
+
+CREATE PROCEDURE `GetStreamerInfo`(IN `sname` VARCHAR(255)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT * FROM streamer WHERE name=sname;
+CREATE PROCEDURE `GetStreamInfo`(IN `sname` VARCHAR(255)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT * FROM stream WHERE streamer_name=sname;
+CREATE PROCEDURE `GetCategoryInfo`(IN `cname` VARCHAR(255)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT * FROM category WHERE category_name=cname;
+CREATE PROCEDURE `GetGameInfo`(IN `gname` VARCHAR(255)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT * FROM game WHERE game_name=gname;
+CREATE PROCEDURE `GetChatInfo`(IN `chid` VARCHAR(255)) NOT DETERMINISTIC CONTAINS SQL SQL SECURITY DEFINER SELECT * FROM stream WHERE chat_ID=chid;
+
+
+-- task 7: drop triggers if exist
+
+drop trigger if exists update_streamer_counts;
+
+-- task 8: create triggers
+
+
+-- -- -- Create a trigger that updates the `follower_count` and `subscriber_count`
+-- -- -- in the `Streamer` table when a new stream is inserted into the `stream` table
+CREATE TRIGGER update_streamer_counts
+AFTER INSERT ON stream
+FOR EACH ROW
+BEGIN
+    UPDATE stream
+    SET follower_count = follower_count + NEW.followers_gained,
+        subscriber_count = subscriber_count + NEW.subscribers_gained
+    WHERE name = NEW.streamer_name;
+END;
+
+--Creates a trigger that increments the streamID in the game table every 
+--time a new stream is entered in the stream table
+CREATE TRIGGER increment_stream_id
+AFTER INSERT ON stream
+FOR EACH ROW
+BEGIN
+    UPDATE game
+    SET streamID = streamID + 1
+    WHERE game_name = NEW.title;
+END;
+
+--Creates a trigger that increments the streamID in the chat table every 
+--time a new stream is entered in the stream table
+CREATE TRIGGER increment_stream_id_chat
+AFTER INSERT ON stream
+FOR EACH ROW
+BEGIN
+    UPDATE chat
+    SET streamID = streamID + 1
+    WHERE chat_ID = NEW.title;
+END;
+
+
+
+
+
+
+
